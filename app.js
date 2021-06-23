6529294d34761de00d9f4593dcf3c02e431a5833
TildaDares <mathildaudufo@gmail.com>
2021-06-23 11:03:31 +0100

    done

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 //Import routes for "catalog" area of site
 var catalogRouter = require("./routes/catalog");
+var compression = require("compression");
+var helmet = require("helmet");
 
 var app = express();
 
 var mongoose = require("mongoose");
-var mongoDB =
-  "mongodb+srv://beautifulchaos:komekelvin@cluster0.fjbko.mongodb.net/local_library?retryWrites=true&w=majority";
+// Set up mongoose connection
+var dev_db_url =
+  "mongodb+srv://beautifulchaos:coolpassword@cluster0.fjbko.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
@@ -26,6 +30,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
