8510dff90dfb00756d6a7d099423e52a005404f5
TildaDares <mathildaudufo@gmail.com>
2021-06-23 11:15:04 +0100

    adjustments

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,30 +6,32 @@ var BookInstance = require("../models/bookinstance");
 var async = require("async");
 
 exports.index = function (req, res) {
-  async.parallel({
-    book_count: function (callback) {
-      Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+  async.parallel(
+    {
+      book_count: function (callback) {
+        Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+      },
+      book_instance_count: function (callback) {
+        BookInstance.countDocuments({}, callback);
+      },
+      book_instance_available_count: function (callback) {
+        BookInstance.countDocuments({ statis: "Available" }, callback);
+      },
+      author_count: function (callback) {
+        Author.countDocuments({}, callback);
+      },
+      genre_count: function (callback) {
+        Genre.countDocuments({}, callback);
+      },
     },
-    book_instance_count: function (callback) {
-      BookInstance.countDocuments({}, callback);
-    },
-    book_instance_available_count: function (callback) {
-      BookInstance.countDocuments({ statis: "Available" }, callback);
-    },
-    author_count: function (callback) {
-      Author.countDocuments({}, callback);
-    },
-    genre_count: function (callback) {
-      Genre.countDocuments({}, callback);
-    },
-    function(err, results) {
+    function (err, results) {
       res.render("index", {
         title: "Local Library Home",
         error: err,
         data: results,
       });
-    },
-  });
+    }
+  );
 };
 
 // Display list of all books.
